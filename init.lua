0dcfa1cf33adcdec821be8ae78c0888d3c1a4266
bingjie6 <bingjie6@staff.weibo.com>
2022-12-06 10:45:26 +0800

    提交nvim custom

---
+++ init.lua
@@ -0,0 +1,20 @@
+local autocmd = vim.api.nvim_create_autocmd
+vim.o.sessionoptions = "curdir,folds,help,tabpages,winpos,buffers,globals"
+vim.cmd("set noswapfile")
+
+vim.o.confirm = true
+vim.opt.title = true
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("NvimTreeClose", {clear = true}),
+  callback = function()
+    local layout = vim.api.nvim_call_function("winlayout", {})
+    if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree" and layout[3] == nil then vim.cmd("quit") end
+  end
+})
+
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
---
+++ plugins/overrides.lua
@@ -0,0 +1,59 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "typescript",
+    "c",
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+M.telescope = {
+  module = "telescope",
+  extensions_list = { "themes", "terms", "session-lens" },
+  extensions= {
+    projects = {
+      projects = {},
+    }
+  },
+  defaults={
+    cache_picker={
+      num_pickers=3,
+    },
+  },
+}
+
+return M
