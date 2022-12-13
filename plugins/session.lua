e9abe4dbf01222aa9dbeb6d17817d458b20964e6
bingjie6 <bingjie6@staff.weibo.com>
2022-12-13 11:55:54 +0800

    php的lsp需要重启

--- plugins/session.lua
+++ plugins/session.lua
@@ -35,6 +35,9 @@ vim.api.nvim_exec(
          let g:session_swap_name =  fnamemodify(v:this_session, ':t')
          call xolox#session#open_cmd(a:name, a:bang, 'OpenSession')
        endif
+       if &filetype == "php"
+         exe "silent! LspRestart"
+       endif
      endfunction
      command! -bar -bang -nargs=? -complete=customlist,xolox#session#complete_names MyOpenSession  call MySessionReload(<q-args>, <q-bang>)
 ]],
